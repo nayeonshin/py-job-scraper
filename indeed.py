d4d2073889306c93447ef1f5e4962746356a7b90
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-19 02:57:24 +0900

    feat: Make spans local variables and add docstrings

--- indeed.py
+++ indeed.py
@@ -3,14 +3,19 @@ import requests
 
 LIMIT = 50
 URL = f'https://www.indeed.com/jobs?q=python&limit={LIMIT}'
-spans = []
 
 
 def parse_spans(response):
+    """
+    Parse spans from response
+    :param response: Response
+    :return: List[Tag]
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     pagination = soup.find('ul', class_='pagination-list')
     pages = pagination.find_all('a')
 
+    spans = []
     temp_spans = []
 
     for page in pages:
@@ -20,8 +25,15 @@ def parse_spans(response):
     else:
         spans.extend(temp_spans[:-1])
 
+    return spans
+
 
 def extract_jobs(last_page_num):
+    """
+    ???
+    :param last_page_num: int
+    :return: List[???]
+    """
     jobs = []
 
     for page in range(last_page_num):
@@ -32,11 +44,15 @@ def extract_jobs(last_page_num):
 
 
 def get_last_page_num():
-    result = requests.get(URL)
-    parse_spans(result)
+    """
+    Get the last page's number at URL
+    :return: int
+    """
+    first_response = requests.get(URL)
+    spans = parse_spans(first_response)
 
     for i in range(200, 901, 100):
-        result = requests.get(URL + f'&start={i}')
-        parse_spans(result)
+        response = requests.get(URL + f'&start={i}')
+        spans = parse_spans(response)
 
     return int(spans[-1].string)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from indeed import *
 
 last_indeed_page = get_last_page_num()
-indeed_jobs = extract_jobs(last_indeed_page)
+print(last_indeed_page)
+# indeed_jobs = extract_jobs(last_indeed_page)
