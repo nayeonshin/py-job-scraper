a75d7d67c9f3c90359488971f4425855da58219e
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-18 20:08:51 +0900

    feat: Get the children nodes of li's on the first page

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+from bs4 import BeautifulSoup
 import requests
 
-indeed_result = requests.get('https://www.indeed.com/jobs?as_and=python&limit=50')
+# Goal: To get all the possible jobs
+# Get each page until aria-level is Next
+# If aria-level is Next, update the requests.get()'s url to
+# ...&start=something
+# If there's no aria-level == Next
+# End the loop
 
-print(indeed_result.text)
+page_index = 0
+is_end_of_pages = False
+li_children = []
+
+indeed_result = requests.get('https://www.indeed.com/jobs?q=python&limit=50')
+indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+pagination = indeed_soup.find('div', class_='pagination')
+pages = pagination('li')
+
+for i in range(len(pages) - 1):  # Excludes last li, which is the next button
+    child = pages[i].findChild()
+    if child.name == 'a':
+        li_children.append(child.find('span', class_='pn'))
+    else:
+        li_children.append(child)
+
+print(li_children)
+
+# while not is_end_of_pages:
+#     if pages[-1].findChild()['aria-label'] == 'Next':  # There are more pages.
+#         indeed_result = requests.get('https://www.indeed.com/jobs?q=python&limit=50'
+#                                      + f'&start={(page_index + 1) * 50}')
+#         indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+#         pagination = indeed_soup.find('div', class_='pagination')
+#         pages = pagination('li')
+#         # print(pages)
+#     else:
+#         is_end_of_pages = True
+#
+#     page_index += 1
+#     if page_index == 2:
+#         break
