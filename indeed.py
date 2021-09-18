7052f8e61b00529f1cdde03354d351e62ff1ccb8
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-19 02:46:15 +0900

    feat: Better Indeed scraper

--- indeed.py
+++ indeed.py
@@ -3,63 +3,40 @@ import requests
 
 LIMIT = 50
 URL = f'https://www.indeed.com/jobs?q=python&limit={LIMIT}'
+spans = []
 
 
-def check_is_first_page_last():
-    is_first_page_last = False
-    max_page_num = 0
+def parse_spans(response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    pagination = soup.find('ul', class_='pagination-list')
+    pages = pagination.find_all('a')
 
-    result = requests.get(URL)
-    soup = BeautifulSoup(result.text, 'html.parser')
-    pagination = soup.find('div', class_='pagination')
-    pages = pagination('li')
+    temp_spans = []
 
-    for i in range(len(pages) - 1):  # Excludes the next button
-        page_num = int(pages[i].findChild().string)
-        if page_num > max_page_num:
-            max_page_num = page_num
-
-    if pages[-1].findChild()['aria-label'] != 'Next':
-        # First page is the last page
-        is_first_page_last = True
-
-    return is_first_page_last, max_page_num, pages
-
-
-def get_page(page_index):
-    return requests.get(URL + f'&start={(page_index + 1) * LIMIT}')
+    for page in pages:
+        temp_spans.append(page.find('span'))
+    if len(str(temp_spans[0])) == 182:  # Current page
+        spans.extend(temp_spans[3:-1])
+    else:
+        spans.extend(temp_spans[:-1])
 
 
 def extract_jobs(last_page_num):
     jobs = []
+
     for page in range(last_page_num):
-        result = get_page(page)
-        print(result.status_code)
+        response = requests.get(f'{URL}&start={page*LIMIT}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+
     return jobs
 
 
-def extract_pages():
-    is_end_of_pages, max_page_num, pages = check_is_first_page_last()
-    page_index = 0
+def get_last_page_num():
+    result = requests.get(URL)
+    parse_spans(result)
 
-    while not is_end_of_pages:
-        if pages[-1].findChild()['aria-label'] == 'Next':  # There are more pages.
-            result = get_page(page_index)
-            soup = BeautifulSoup(result.text, 'html.parser')
-            pagination = soup.find('div', class_='pagination')
-            pages = pagination('li')
-            for i in range(1, len(pages)):  # Excludes the previous button
-                li_child = pages[i].findChild().string
-                if li_child is None:
-                    continue
-                else:
-                    page_num = int(li_child)
-                    if page_num > max_page_num:
-                        max_page_num = page_num
-        else:
-            is_end_of_pages = True
+    for i in range(200, 901, 100):
+        result = requests.get(URL + f'&start={i}')
+        parse_spans(result)
 
-        page_index += 1
-
-    return max_page_num
-
+    return int(spans[-1].string)
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from indeed import *
 
-last_indeed_page = extract_pages()
-print(last_indeed_page)
-
-# indeed_jobs = extract_jobs(last_indeed_page)
+last_indeed_page = get_last_page_num()
+indeed_jobs = extract_jobs(last_indeed_page)
